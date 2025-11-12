69f3f0e82f0d9f57000bee4b9e08436c2a86f4a5
KaveriKumbhar <kaverikumbhar200415@gmail.com>
2025-11-12 16:12:02 +0530

    fixed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser:true, useUnifiedTopology:true })
+mongoose.connect(process.env.MONGO_URI)
   .then(()=> {
     console.log('Mongo connected')
     app.listen(PORT, ()=> console.log('Server running on', PORT))
